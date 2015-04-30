cf7c5f39ba1ece40dc7f1c31c9b3caf9697d1930
Carol McDonald <caroljmcdonald@gmail.com>
2015-04-30 16:15:49 -0400

    add wordcount and pom file
    add java and scala word count example and pom file

---
+++ src/main/java/example/wordcount/JavaWordCount.java
@@ -0,0 +1,76 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package example.wordcount;
+
+import java.util.Arrays;
+
+import scala.Tuple2;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.FlatMapFunction;
+import org.apache.spark.api.java.function.Function2;
+import org.apache.spark.api.java.function.PairFunction;
+
+public class JavaWordCount {
+
+    public static void main(String[] args) throws Exception {
+        String inputFile = args[0];
+        String outputFile = args[1];
+        // Create a Java Spark Context.
+        SparkConf conf = new SparkConf().setAppName("wordCount");
+        JavaSparkContext sc = new JavaSparkContext(conf);
+        // Load our input data.
+        JavaRDD<String> input = sc.textFile(inputFile);
+
+        // map/split each line to multiple words
+        JavaRDD<String> words = input.flatMap(
+                new FlatMapFunction<String, String>() {
+                    @Override
+                    public Iterable<String> call(String x) {
+                        return Arrays.asList(x.split(" "));
+                    }
+                }
+        );
+
+        // Turn the words into (word, 1) pairs
+        JavaPairRDD<String, Integer> wordOnePairs = words.mapToPair(
+                new PairFunction<String, String, Integer>() {
+                    @Override
+                    public Tuple2<String, Integer> call(String x) {
+                        return new Tuple2(x, 1);
+                    }
+                }
+        );
+        // reduce add the pairs by key to produce counts
+        JavaPairRDD<String, Integer> counts = wordOnePairs.reduceByKey(
+                new Function2<Integer, Integer, Integer>() {
+                    @Override
+                    public Integer call(Integer x, Integer y) {
+                        return x + y;
+                    }
+                }
+        );
+
+        // Save the word count back out to a text file, causing evaluation.
+        counts.saveAsTextFile(outputFile);
+        System.out.println(counts.collect());
+    }
+}
